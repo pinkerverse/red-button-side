25ee12e812bde421aad38f3520c34c3267657be3
pinkerverse <s.eichhorn.se@gmail.com>
2026-04-22 17:27:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const button = document.getElementById("redButton");
 const sound = new Audio("sound.mp3");
+const text = document.getElementById("warningText");
 
 button.addEventListener("click", () => {
-  sound.currentTime = 0; // restart sound if clicked fast
+  sound.currentTime = 0;
   sound.play();
+
+  text.innerText = "I TOLD YOU NOT TO 😡";
 });
